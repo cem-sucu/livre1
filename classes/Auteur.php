6dca928090ce149ae735d470dfd988138e8bf579
cem-sucu <cems.cems.17@gmail.com>
2023-08-10 11:19:22 +0200

    on ajoute les attribut, le constructor, les getters et setter ainsi que la function _toString dans le fichier Auteur

--- classes/Auteur.php
+++ classes/Auteur.php
@@ -1,7 +1,66 @@
 <?php
 
+// TODO création de la classe Auteuyr
 class Auteur {
 
+    //TODO on créé les attributs
+    private string $nom;
+    private string $prenom;
+
+    //TODO  on créée le __constructor
+    public function __construct(string $nom, string $prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    
+
+    //TODO on ajout les getter et setter de chaque attribut
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    //TODO on crée la _toString
+    public function toString() {
+        return $this->getPrenom() . " ".$this->getNom();
+    }
 }
 
 
